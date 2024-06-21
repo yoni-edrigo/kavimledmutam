a600ea160ad50686eef12f8b20f54312d04cd00e
yoni vasilevski <info@edrigo.com>
2024-06-22 01:46:57 +0300

    removed local stored video

--- src/components/landing-video.tsx
+++ src/components/landing-video.tsx
@@ -1,9 +1,16 @@
-import landingVid from '../assets/landingVid.webm';
 import '../styles/landing-video.css';
 export function LandingVideo() {
   return (
     <section className="video-section lg:py-7">
-      <video src={landingVid} controls autoPlay muted loop />
+      <video
+        src={
+          'https://video.wixstatic.com/video/557f26_f183b7fde5bb4388896e2721da8c6a6a/480p/mp4/file.mp4'
+        }
+        controls
+        autoPlay
+        muted
+        loop
+      />
     </section>
   );
 }
